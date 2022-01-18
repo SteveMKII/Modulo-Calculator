5afd1d748f90343edd67335ba2311095494dbb15
SteveMKII <stevepura@gmail.com>
2022-01-17 20:10:20 -0800

    Added conditionals to check that both values are non null and integers, improved functional decomposition

--- app/src/main/java/com/modulocalculator/MainActivity.kt
+++ app/src/main/java/com/modulocalculator/MainActivity.kt
@@ -12,10 +12,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val calculate: Button = findViewById(R.id.result)
         calculate.setOnClickListener{
-            val numberView: TextView = findViewById(R.id.number)
-            val modView: TextView = findViewById(R.id.mod)
-            val number: Int = numberView.text.toString().toInt()
-            val mod: Int = modView.text.toString().toInt()
+            val number = getNumber()
+            val mod = getModulo()
+            showResult(number, mod)
+        }
+    }
+
+     fun getNumber(): Int? {
+        val numberView: TextView = findViewById(R.id.number)
+        return numberView.text.toString().toIntOrNull()
+    }
+
+
+    fun getModulo(): Int? {
+        val modView: TextView = findViewById(R.id.mod)
+        return modView.text.toString().toIntOrNull()
+    }
+
+
+    fun showResult(number: Int?, mod: Int?){
+        if (number !is Int){
+            Toast.makeText(applicationContext, "Please enter an integer for number",
+                Toast.LENGTH_SHORT).show()
+        }
+        else if (mod !is Int){
+            Toast.makeText(applicationContext, "Please enter an integer for mod",
+                Toast.LENGTH_SHORT).show()
+        }
+        else{
             val result: String = (number % mod).toString()
             val duration = Toast.LENGTH_SHORT
             val toast = Toast.makeText(applicationContext, result, duration)
@@ -23,3 +47,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
