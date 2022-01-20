241a6f0ae4e23cad581948f45723a835d19fe877
SteveMKII <stevepura@gmail.com>
2022-01-20 01:05:25 -0800

    renamed intent to result in ResultActivity

--- app/src/main/java/com/modulocalculator/ResultActivity.kt
+++ app/src/main/java/com/modulocalculator/ResultActivity.kt
@@ -1,17 +1,15 @@
 package com.modulocalculator
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import android.widget.Toast
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
-        val intent: String? = intent.getStringExtra("result")
+        val result: String? = intent.getStringExtra("result")
         val resultView:  TextView = findViewById(R.id.resultVal)
-        resultView.text = intent
+        resultView.text = result
     }
 }
